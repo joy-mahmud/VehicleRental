8e19ebfd36514d977ea68edaae9112a2c844d975
joy-mahmud <joymahmudiucse@gmail.com>
2024-10-26 23:54:38 +0600

    adding users system to the project

--- VehicalRental.py
+++ VehicalRental.py
@@ -1,8 +1,20 @@
+
 class Vehicle:
     def __init__(self,name,vehicle_type):
         self.name=name
         self.vehicle_type=vehicle_type
         self.rented=False
+userList=[]
+class CreateUser:
+    def __init__(self,id,username,role):
+        userList.append(User(id,username,role))
+
+class User:
+    def __init__(self,id,username,role):
+        self.id=id
+        self.username=username
+        self.role=role
+
 
 class Car(Vehicle):
     def __init__(self,name):
@@ -18,7 +30,19 @@ class RentalService:
         bmw=Car("bmw")
         yamaha = Bike("yamaha")
         self.vehicles=[lamborgini,bmw,yamaha]
-
+        self.users = {"joy": "admin", "rohim": "user"}
+    def add_vehicles(self,username,vehicle_name,vehicle_type):
+        try:
+            if(self.users[username]=='admin'):
+                if (vehicle_type == "car"):
+                    new_vehicle = Car(vehicle_name)
+                    self.vehicles.append(new_vehicle)
+                elif(vehicle_type=="bike"):
+                    new_vehice = Bike(vehicle_name)
+                    self.vehicles.append(new_vehice)
+            elif(self.users[username]=="user"): print("Only admin can add vehicles")
+        except:
+            print("you are not registered yet")
     def rent_vehicle(self,name):
         for vehicle in self.vehicles:
             if vehicle.name == name and not vehicle.rented:
@@ -31,7 +55,16 @@ class RentalService:
 
 rental_service=RentalService()
 # for rent a car
-rental_service.rent_vehicle('lambhorghini')
-rental_service.rent_vehicle('bmw')
-rental_service.rent_vehicle('lambhorghini')
-rental_service.rent_vehicle('yamaha')
+# rental_service.rent_vehicle('lambhorghini')
+# rental_service.rent_vehicle('bmw')
+# rental_service.rent_vehicle('lambhorghini')
+# rental_service.rent_vehicle('yamaha')
+
+# rental_service.add_vehicles('joy','ferrari','car')
+#
+# rental_service.rent_vehicle('ferrari')
+# rental_service.add_vehicles('rohim','ferrari','car')
+
+CreateUser(101,"joy","admin")
+CreateUser(102,"mahmud","user")
+print(userList)
